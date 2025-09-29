a3cbafc09e5f60f385105b59b1744f8e6ad53f37
Abdulaziz Adam <azizpob11@gmail.com>
2025-09-29 19:43:43 +0300

    second commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 
+
 from pages import views
 
 urlpatterns = [
   path("", views.home, name="home"),
+  path("about", views.about, name="about"),
+  path("services", views.services, name="services"),
+  path("contact", views.contact, name="contact"),
+  path("cars", views.cars, name="cars"),
   ]
